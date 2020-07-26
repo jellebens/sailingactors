0e0b7a12ec26f0cf08da17e9627dc43fe3056624
Jelle Bens <jelle.bens@portima.com>
2020-07-27 01:33:05 +0200

    Actor outline...

---
+++ src/SailingActors/SailingActors.Shared/IShoppingCart.cs
@@ -0,0 +1,10 @@
+﻿using Dapr.Actors;
+using System;
+
+namespace SailingActors.Shared
+{
+    public interface IShoppngCart: IActor
+    {
+        public void Submit();
+    }
+}
---
+++ src/SailingActors/SalilingActors.Actors/ShoppingCart.cs
@@ -0,0 +1,38 @@
+﻿using Dapr.Actors;
+using Dapr.Actors.Runtime;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using SailingActors.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalilingActors.Actors
+{
+    public class ShoppingCart : Actor, IShoppngCart
+    {
+        private readonly ILogger<ShoppingCart> _Logger;
+
+        public ShoppingCart(ActorService actorService, ActorId actorId, IActorStateManager actorStateManager = null) : base(actorService, actorId, actorStateManager)
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddConsole(config => {
+                        config.Format = ConsoleLoggerFormat.Systemd;
+                        config.TimestampFormat = "[HH:mm:ss] ";
+                    });
+            });
+
+            _Logger = loggerFactory.CreateLogger<ShoppingCart>();
+        }
+
+        public void Submit()
+        {
+            
+        }
+    }
+}
